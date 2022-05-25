755e50d260b9117bc01ed47d026093808b6d5564
Roberto Murga <rmurgagdam1b@gmail.com>
2022-05-25 13:06:10 +0200

    Clase Jugador colocada en una clase propia

--- src/Juego.java
+++ src/Juego.java
@@ -75,39 +75,3 @@ public class Juego
         } while(finDeJuego!=true);
     }
 }
-/**
- *
- */
-class Jugador{
-   
-    /**
-     * Escoge piedra, papel o tijera al azar
-     */
-    public String opcionAlAzar()
-    {
-        String opcion="";
-        Integer c = (int)(Math.random()*3);
-        switch(c){
-            case 0:
-            	opcion=("piedra");
-                break;
-            case 1:
-            	opcion=("papel");
-                break;
-            case 2:
-            	opcion=("tijeras");
-        }
-        return opcion;
-    }
-    public void setExitos()
-    {
-        exitos++;
-    }
-    public int getExitos()
-    {
-        return(exitos);
-    }
-    
-    int exitos;      // número de partidas ganadas
-    int winTotal;
-}
---
+++ src/Jugador.java
@@ -0,0 +1,38 @@
+package ende;
+
+/**
+ *
+ */
+class Jugador{
+
+    /**
+     * Escoge piedra, papel o tijera al azar
+     */
+    public String opcionAlAzar()
+    {
+        String opcion="";
+        Integer c = (int)(Math.random()*3);
+        switch(c){
+            case 0:
+                opcion=("piedra");
+                break;
+            case 1:
+                opcion=("papel");
+                break;
+            case 2:
+                opcion=("tijeras");
+        }
+        return opcion;
+    }
+    public void setExitos()
+    {
+        exitos++;
+    }
+    public int getExitos()
+    {
+        return(exitos);
+    }
+
+    int exitos;      // número de partidas ganadas
+    int winTotal;
+}
